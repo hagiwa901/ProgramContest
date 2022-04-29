14caf4fceb5ad50212582d96b1f40e3781d4cd7d
hagiwa901 <kedama4156991@gmail.com>
2022-04-29 22:41:50 +0900

    first commit

---
+++ SampleProgram/BitSearch.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORR(i, m, n) for (int i = m; i >= n; i--)
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define ll long long
+#define ull unsigned long long
+#define pb(a) push_back(a)
+#define INF 100000000001
+using namespace std;
+typedef pair<int, int> P;
+typedef pair<ll, ll> LP;
+typedef pair<int, P> PP;
+typedef pair<ll, LP> LPP;
+ 
+int dy[] = {0, -1, 0, 1};
+int dx[] = {1, 0, -1, 0};
+ 
+ 
+int main() {
+    int n = 3;
+
+    // {0, 1, ..., n-1} の部分集合の全探索
+    for (int bit = 0; bit < (1<<n); ++bit) {
+        vector<int> S;
+        for (int i = 0; i < n; ++i) {
+            if (bit & (1<<i)) { // 列挙に i が含まれるか
+                S.push_back(i);
+            }
+        }
+
+        cout << bit << ": {";
+        for (int i = 0; i < (int)S.size(); ++i) {
+            cout << S[i] << " ";
+        }
+        cout << "}" << endl;
+    }
+}
