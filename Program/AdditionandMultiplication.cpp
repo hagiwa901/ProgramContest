fb68a4f48c05a43354c00372bbbb2a470551c026
hagiwa901 <kedama4156991@gmail.com>
2022-05-04 17:47:45 +0900

    add

---
+++ Program/AdditionandMultiplication.cpp
@@ -0,0 +1,37 @@
+// https://atcoder.jp/contests/abc076/tasks/abc076_b
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORR(i, m, n) for (int i = m; i >= n; i--)
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define ll long long
+#define ull unsigned long long
+#define pb(a) push_back(a)
+#define INF 100000000001
+using namespace std;
+typedef pair<int, int> P;
+typedef pair<ll, ll> LP;
+typedef pair<int, P> PP;
+typedef pair<ll, LP> LPP;
+ 
+int dy[] = {0, -1, 0, 1};
+int dx[] = {1, 0, -1, 0};
+ 
+ 
+int main()
+{
+    int N,K,num=1;
+    cin >> N >> K;
+
+    for(int i = 0; i < N; i++){
+        if(num*2 <= num + K){
+            num *= 2;
+        }
+        else{
+            num += K;
+        }       
+    }
+    cout << num << endl;
+}
