661bf90e1505af3b3250d57609d94c0f64a0a2b6
hagiwa901 <kedama4156991@gmail.com>
2022-05-05 23:23:56 +0900

    add

---
+++ Program/BasicBinarySearch.cpp
@@ -0,0 +1,40 @@
+//https://atcoder.jp/contests/typical-algorithm/tasks/typical_algorithm_a?lang=ja
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORR(i, m, n) for (int i = m; i >= n; i--)
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define ll long long
+#define ull unsigned long long
+#define pb(a) push_back(a)
+#define INF 100000000001
+using namespace std;
+typedef pair<int, int> P;
+typedef pair<ll, ll> LP;
+typedef pair<int, P> PP;
+typedef pair<ll, LP> LPP;
+ 
+int dy[] = {0, -1, 0, 1};
+int dx[] = {1, 0, -1, 0};
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    REP(i, n) cin >> a[i];
+    int l=0, r=n-1;
+    if(a[r] < k){
+        cout << -1 << endl;
+        return 0;
+    }
+    while(r-l>1){
+        int m = l + (r-l)/2;
+        if(a[m] > k) r = m;
+        else l = m;
+        // cout << a[l] << " " << a[r] << endl;
+    }
+    cout << l+1 << endl;
+}
