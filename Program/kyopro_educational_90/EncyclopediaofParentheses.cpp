3501679f36091878cb943452aa374e76d5c9692a
hagiwa901 <kedama4156991@gmail.com>
2022-05-18 23:33:59 +0900

    add

---
+++ Program/kyopro_educational_90/EncyclopediaofParentheses.cpp
@@ -0,0 +1,44 @@
+// https://atcoder.jp/contests/typical90/tasks/typical90_b
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORR(i, m, n) for (int i = m; i >= n; i--)
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define ll long long
+#define ull unsigned long long
+#define pb(a) push_back(a)
+#define INF 100000000001
+using namespace std;
+typedef pair<int, int> P;
+typedef pair<ll, ll> LP;
+typedef pair<int, P> PP;
+typedef pair<ll, LP> LPP;
+ 
+int dy[] = {0, -1, 0, 1};
+int dx[] = {1, 0, -1, 0};
+
+int main()
+{
+    int n;
+    cin >> n;
+    set<string> k;
+    for (int bit = 0; bit < (1<<n); ++bit) {
+        string s;
+        int m=0;
+        for (int i = 0; i < n; i++) {
+            if (bit & (1<<i)) { // 列挙に i が含まれるか
+                s += '(';
+                m++;
+            }
+            else{
+                s += ')';
+                m--;
+            }
+            if(m < 0) break;
+        }
+        if(m == 0) k.insert(s);
+    }
+    for(auto itr=k.begin(); itr!=k.end(); ++itr) cout << *itr << endl;
+}
