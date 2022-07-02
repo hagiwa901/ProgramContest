527294958e9abeb0e8cae408e94271ddf6624e27
hagiwa901 <kedama4156991@gmail.com>
2022-07-02 23:01:19 +0900

    add

---
+++ Program/ABC258/A_When.cpp
@@ -0,0 +1,28 @@
+// https://atcoder.jp/contests/abc258/tasks/abc258_a
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORR(i, m, n) for (int i = m; i >= n; i--)
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define ll long long
+#define ull unsigned long long
+#define pb(a) push_back(a)
+#define INF 100000000001
+using namespace std;
+typedef pair<int, int> P;
+typedef pair<ll, ll> LP;
+typedef pair<int, P> PP;
+typedef pair<ll, LP> LPP;
+ 
+int dy[] = {0, -1, 0, 1};
+int dx[] = {1, 0, -1, 0};
+
+int main() {
+    int k;
+    cin >> k;
+    string hour = to_string(21+k/60), minu=to_string(k%60);
+    if(k%60 < 10) minu = "0" + minu;
+    cout << hour << ":" << minu << endl;
+}
