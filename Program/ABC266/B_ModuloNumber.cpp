6fe832aea98fe97b104b31f2b0bc8ce293860df1
hagiwa901 <kedama4156991@gmail.com>
2022-08-27 22:46:53 +0900

    add

---
+++ Program/ABC266/B_ModuloNumber.cpp
@@ -0,0 +1,31 @@
+// https://atcoder.jp/contests/abc266/tasks/abc266_b
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+#define FORR(i, m, n) for (int i = m; i >= n; i--)
+#define SORT(v, n) sort(v, v + n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define ll long long
+#define ull unsigned long long
+#define pb(a) push_back(a)
+#define INF 100000000001
+using namespace std;
+typedef pair<int, int> P;
+typedef pair<ll, ll> LP;
+typedef pair<int, P> PP;
+typedef pair<ll, LP> LPP;
+ 
+int dy[] = {-1, 0, -1, 0, 1, 1, 1, -1};
+int dx[] = {-1, 1, 0, -1, 0, 1, -1, 1};
+
+int main() {
+    ll n;
+    cin >> n;
+    for(int i=0; i<998244353; i++){
+        if((n - i) % 998244353 == 0){
+            cout << i << endl;
+            break;
+        }
+    }
+}
---
+++ Program/ABC266/Readme.md
@@ -0,0 +1,5 @@
+# コンテスト名
+AtCoder Beginner Contest 266
+
+## URL
+https://atcoder.jp/contests/abc266
